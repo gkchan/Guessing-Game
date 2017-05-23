4da34fb700f94c38039f8e3f0eb88667fab0dee7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 16:03:20 -0700

    Added parameter for edge cases.

--- game.py
+++ game.py
@@ -14,11 +14,15 @@ while True:
     guess = int(raw_input("Guess a number between 1 and 100."))
     #print int(guess)
     tries += 1
-    if guess > number:
+    if guess > 100 or guess < 1:
+        print "Please enter a number between 1 and 100."
+    elif guess > number:
         print "Guess is too high. Try again."
     elif guess < number:
         print "Guess is too low. Try again."
-    else:
+    elif guess == number:
         print "Congrats! %s, you guessed the number!" % (name)
         print "It took you {} tries!" .format(tries)
+    else:
+        print "Error"
         
