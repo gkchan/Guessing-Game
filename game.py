41a6d0bdb6de259acbe1eb1b7d9853ebc882047a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 13:02:56 -0700

    fixed guessing issue

--- game.py
+++ game.py
@@ -4,4 +4,14 @@ import random
 
 name = raw_input("What's your name?")
 print "%s, welcome to the guessing game." % (name)
-number = random.randint(1,100)
+number = random.randint(1, 100)
+print number
+while True:
+    print number
+
+    guess = raw_input("Guess a number between 1 and 100.")
+    print int(guess)
+    if guess > number:
+        print "Guess is too high. Try again."
+    elif guess < number:
+        print "Guess is too low. Try again."
