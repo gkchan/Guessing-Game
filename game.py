628512c2459eb5fdd6c02779f68e3a7ad97c7e97
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 15:46:40 -0700

    Completed game

--- game.py
+++ game.py
@@ -2,16 +2,23 @@
 
 import random
 
+tries = 0
+
 name = raw_input("What's your name?")
 print "%s, welcome to the guessing game." % (name)
 number = random.randint(1, 100)
 print number
 while True:
-    print number
+    #print number
 
-    guess = raw_input("Guess a number between 1 and 100.")
-    print int(guess)
+    guess = int(raw_input("Guess a number between 1 and 100."))
+    #print int(guess)
+    tries += 1
     if guess > number:
         print "Guess is too high. Try again."
     elif guess < number:
         print "Guess is too low. Try again."
+    else:
+        print "Congrats! %s, you guessed the number!" % (name)
+        print "It took you {} tries!" .format(tries)
+        
