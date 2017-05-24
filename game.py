480b8e2631adc33f8dadf7f521cd1888dd820818
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 18:02:15 -0700

    we broke it, guessing game

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 """A number-guessing game."""
 
 import random
+import math
+    
 
 tries = 0
 
@@ -10,19 +12,25 @@ number = random.randint(1, 100)
 #print number
 while True:
     #print number
-
-    guess = int(raw_input("Guess a number between 1 and 100."))
-    #print int(guess)
+    try:
+        if guess_1.isdigit
+            guess_1 = raw_input("Guess a number between 1 and 100.")
+        if guess_1 != 
+            guess_2 = math.floor(guess_1)
+            print "If your number is not an integer we are rounding it down!"
+        else:
+            print "Error"
+    except TypeError:
+        print "Please enter an integer between 1 and 100."
+        continue
     tries += 1
-    if guess > 100 or guess < 1:
-        print "Please enter a number between 1 and 100."
-    elif guess > number:
-        print "Guess is too high. Try again."
-    elif guess < number:
-        print "Guess is too low. Try again."
-    elif guess == number:
-        print "Congrats! %s, you guessed the number!" % (name)
-        print "It took you {} tries!" .format(tries)
-    else:
-        print "Error"
-        
+    # if guess_2 > 100 or guess_2 < 1:
+    #     print "Please enter a number between 1 and 100."
+    # elif guess_2 > number:
+    #     print "Guess is too high. Try again."
+    # elif guess_2 < number:
+    #     print "Guess is too low. Try again."
+    # else:
+    #     print "Congrats! %s, you guessed the number!" % (name)
+    #     print "It took you {} tries!" .format(tries)
+   
